4ca63fa7b350be01e5a9d89374e30c78860c9025
zwf <zwfxiaxue@163.com>
2024-04-23 14:48:54 +0800

    test

--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 @csrf_exempt
 def home(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world")
 
 
